c3844bff60e1879dd9f10e66856ca478b2ffc4f2
Emmanuel Branlard <ebra@dtu.dk>
2015-10-22 20:48:18 +0200

    _adding standard C int

--- _unit_tests/fortran-c/test_c_precision.c
+++ _unit_tests/fortran-c/test_c_precision.c
@@ -1,7 +1,7 @@
-/* This program computes the size of variable using sizeof operator.*/
 
 #include <stdio.h>
 #include <limits.h> // for CHAR_BIT
+#include <stdint.h> // for standards int types
 
 int main(){
     // The line below wont compile if a double is not 64 bits
@@ -11,11 +11,15 @@ int main(){
     float b;
     double c;
     char d;
-    printf("       Size of byte  : %2d bits\n",CHAR_BIT);
-    printf("       Size of char  : %2d bits\n" ,sizeof(d)*CHAR_BIT);
-    printf("       Size of int   : %2d bits\n",sizeof(a)*CHAR_BIT);
-    printf("       Size of float : %2d bits\n",sizeof(b)*CHAR_BIT);
-    printf("       Size of double: %2d bits\n",sizeof(c)*CHAR_BIT);
+    int8_t  i8;
+    int32_t i32;
+    printf("       Size of byte   : %2d bits\n",CHAR_BIT);
+    printf("       Size of char   : %2d bits\n" ,sizeof(d)*CHAR_BIT);
+    printf("       Size of int8_t : %2d bits\n",sizeof(i8)*CHAR_BIT);
+    printf("       Size of int32_t: %2d bits\n",sizeof(i32)*CHAR_BIT);
+    printf("       Size of int    : %2d bits\n",sizeof(a)*CHAR_BIT);
+    printf("       Size of float  : %2d bits\n",sizeof(b)*CHAR_BIT);
+    printf("       Size of double : %2d bits\n",sizeof(c)*CHAR_BIT);
     
     printf("[ OK ] C double is 64 bits\n");
     return 0;
